c9d9d153a6414607ec1b4a60f1f6521f33aa08ee
王思旺 <wangsw@sit.com.cn>
2018-12-18 18:37:19 +0800

    增加当前热力信息

--- src/main/java/com/xatu/temperature/controller/QueryPage.java
+++ src/main/java/com/xatu/temperature/controller/QueryPage.java
@@ -1,5 +1,7 @@
 package com.xatu.temperature.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.xatu.temperature.service.QueryPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -35,10 +37,12 @@ public class QueryPage {
         return currInfo;
     }
 
-//    @GetMapping("/getCurrInfoByDeviceId")
-//    @ResponseBody
-//    public  String getCurrInfoByDeviceId(@RequestParam String device_id){
-//        String currInfo = queryPageService.getCurrInfoByDeviceId(device_id);
-//        return currInfo;
-//    }
+    @GetMapping("/getCurrReLiInfo")
+    @ResponseBody
+    public  String getCurrReLiInfo(){
+        String s = "[{\"lng\":109.004393,\"lat\":34.386154,\"count\":180},{\"lng\":109.010058,\"lat\":34.378221,\"count\":203},{\"lng\":109.013147,\"lat\":34.380842,\"count\":181},{\"lng\":109.012032,\"lat\":34.374962,\"count\":179},{\"lng\":109.013662,\"lat\":34.364123,\"count\":176},{\"lng\":108.998728,\"lat\":34.386296,\"count\":184},{\"lng\":108.997526,\"lat\":34.378716,\"count\":173},{\"lng\":108.995295,\"lat\":34.368444,\"count\":186},{\"lng\":108.983278,\"lat\":34.385162,\"count\":177},{\"lng\":108.981819,\"lat\":34.380275,\"count\":172},{\"lng\":108.982163,\"lat\":34.371987,\"count\":187},{\"lng\":108.981304,\"lat\":34.369578,\"count\":181},{\"lng\":108.975296,\"lat\":34.386012,\"count\":176},{\"lng\":108.972464,\"lat\":34.379141,\"count\":179},{\"lng\":108.972464,\"lat\":34.374891,\"count\":176},{\"lng\":108.971691,\"lat\":34.371278,\"count\":184},{\"lng\":108.972635,\"lat\":34.368373,\"count\":184},{\"lng\":108.98654,\"lat\":34.379496,\"count\":189},{\"lng\":108.983879,\"lat\":34.381833,\"count\":184},{\"lng\":108.974695,\"lat\":34.378929,\"count\":176}]";
+        JSONArray parse = (JSONArray) JSONObject.parse(s);
+        return parse.toJSONString();
+    }
+
 }
