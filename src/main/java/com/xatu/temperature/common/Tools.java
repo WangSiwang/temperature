708dcd1188c2003937959d37e6e55d5023587bda
王思旺 <wangsw@sit.com.cn>
2019-01-03 17:55:21 +0800

    增加当前热力信息

---
+++ src/main/java/com/xatu/temperature/common/Tools.java
@@ -0,0 +1,51 @@
+package com.xatu.temperature.common;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 3 on 2019/1/3.
+ */
+public class Tools {
+
+    public static void main(String[] args) {
+        File f = new File("F:\\项目\\温度检测项目\\temperature\\src\\main\\resources\\china_sites_20180101.csv");
+        File b = new File("F:\\项目\\温度检测项目\\temperature\\src\\main\\resources\\站点列表-2018.11.08起.csv");
+
+        try {
+            if (f.exists()) {
+                BufferedReader bf = new BufferedReader(new FileReader(f));
+                BufferedReader bb = new BufferedReader(new FileReader(b));
+                String line;
+                List<String> titles = new ArrayList<>();
+                while ((line = bb.readLine()) !=null) {
+                    titles.add(line);
+                }
+
+                List<String> datas = new ArrayList<>();
+                line = null;
+                while ((line = bf.readLine()) !=null) {
+                    for (String title : titles) {
+                        if (line.contains(title.split(",")[0])) {
+                            line = line.replaceAll(title.split(",")[0], title);
+                            datas.add(line);
+                            break;
+                        }
+                    }
+                }
+                for (String data : datas) {
+                    String[] split = data.split(",");
+
+
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
